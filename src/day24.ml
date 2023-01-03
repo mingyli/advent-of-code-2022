04105f5fa2c0f1f7ae2923bcd79dc3a8028090ba
Ming <mingyli34@gmail.com>
2023-01-03 11:59:14 -0500

    Day 24 b

--- src/day24.ml
+++ src/day24.ml
@@ -39,6 +39,7 @@ module Input = struct
   ;;
 
   let goal t = t.height - 1, t.width - 2
+  let start _ = 0, 1
 
   let is_border t (r, c) =
     match r, c with
@@ -73,40 +74,74 @@ module Input = struct
   ;;
 end
 
+type stage =
+  | First
+  | Second
+  | Third
+[@@deriving hash, sexp, compare]
+
 let solve (input : Input.t) =
-  let q = Queue.singleton ((0, 1), 0, input) in
   let module Key = struct
-    type t = Vec2.t * int [@@deriving hash, sexp, compare]
+    module T = struct
+      type t = Vec2.t * int * stage [@@deriving hash, sexp, compare]
+    end
+
+    include T
+    include Hashable.Make (T)
   end
   in
-  let already_added = Hash_set.of_list (module Key) [ (0, 1), 0 ] in
+  let q =
+    let q = Key.Hash_queue.create () in
+    Hash_queue.enqueue_back_exn q ((0, 1), 0, First) ((0, 1), 0, First);
+    q
+  in
+  let blizzard_by_step =
+    let seq =
+      Sequence.unfold ~init:input ~f:(fun input ->
+        let next_guy = Input.{ input with blizzards = Input.next_blizzards input } in
+        Some (input, next_guy))
+    in
+    let l = Sequence.take seq 999 |> Sequence.to_list in
+    List.mapi l ~f:(fun i t -> i, t) |> Int.Map.of_alist_exn
+  in
   let answer = Set_once.create () in
-  while (not (Queue.is_empty q)) && Set_once.is_none answer do
-    let pos, steps, t = Queue.dequeue_exn q in
+  while (not (Hash_queue.is_empty q)) && Set_once.is_none answer do
+    let pos, steps, stage = Hash_queue.dequeue_front_exn q in
     (* print_s [%message (pos : Vec2.t) (steps : int) (t.blizzards : Input.blizzard list)]; *)
     (* print_s [%message (pos : Vec2.t) (steps : int)]; *)
-    if Vec2.(pos = Input.goal t)
-    then Set_once.set_exn answer [%here] steps
-    else (
+    match stage, pos with
+    | Third, pos when Vec2.(pos = Input.goal input) ->
+      Set_once.set_exn answer [%here] steps
+    | First, pos when Vec2.(pos = Input.goal input) ->
+      let (_ : [ `Key_already_present | `Ok ]) =
+        Hash_queue.enqueue_back q (pos, steps, Second) (pos, steps, Second)
+      in
+      ()
+    | Second, pos when Vec2.(pos = Input.start input) ->
+      let (_ : [ `Key_already_present | `Ok ]) =
+        Hash_queue.enqueue_back q (pos, steps, Third) (pos, steps, Third)
+      in
+      ()
+    | _ ->
       let steps = steps + 1 in
-      let blizzards = Input.next_blizzards t in
+      let t = Map.find_exn blizzard_by_step steps in
       let is_blizzard pos =
-        List.map blizzards ~f:(fun blizzard -> blizzard.pos)
+        List.map t.blizzards ~f:(fun blizzard -> blizzard.pos)
         |> List.exists ~f:(fun p -> Vec2.(p = pos))
       in
-      let t = Input.{ t with blizzards } in
       let candidates =
         [ right; down; 0, 0; up; left ]
         |> List.map ~f:(fun dir -> Vec2.(dir + pos))
-        |> List.filter ~f:(fun (r, c) -> r >= 0 && c >= 0)
+        |> List.filter ~f:(fun (r, c) -> r >= 0 && c >= 0 && r < t.height && c < t.width)
         |> List.filter ~f:(Fn.non (Input.is_border t))
         |> List.filter ~f:(fun pos -> not (is_blizzard pos))
       in
-      List.filter candidates ~f:(fun candidate ->
-        not (Hash_set.mem already_added (candidate, steps)))
+      List.filter candidates ~f:(fun _candidate -> true)
       |> List.iter ~f:(fun candidate ->
-           Hash_set.add already_added (candidate, steps);
-           Queue.enqueue q (candidate, steps, t)))
+           let (_ : [ `Key_already_present | `Ok ]) =
+             Hash_queue.enqueue_back q (candidate, steps, stage) (candidate, steps, stage)
+           in
+           ())
   done;
   Set_once.get_exn answer [%here]
 ;;
@@ -118,20 +153,6 @@ let run which =
 ;;
 
 let%expect_test _ =
-  let input =
-    Input.of_string {|#.#####
-#.....#
-#>....#
-#.....#
-#...v.#
-#.....#
-#####.#|}
-  in
-  print_s [%sexp (solve input : int)];
-  [%expect {| 10 |}]
-;;
-
-let%expect_test _ =
   let input = Input.of_string {|#.######
 #>>.<^<#
 #.<..<<#
@@ -139,13 +160,11 @@ let%expect_test _ =
 #<^v^^>#
 ######.#|} in
   print_s [%sexp (solve input : int)];
-  [%expect {| 18 |}]
+  [%expect {| 54 |}]
 ;;
 
 (* let%expect_test _ =
   let input = In_channel.read_all "../input/input24.txt" |> Input.of_string in
   print_s [%sexp (solve input : int)];
-  [%expect {|
-    ((pos (19 122)) (password 20494))
-    20494 |}]
+  [%expect {| 816 |}]
 ;; *)
